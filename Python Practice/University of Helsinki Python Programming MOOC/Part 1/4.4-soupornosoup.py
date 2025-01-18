836c39dc6e6410ad34753e43ab23469f2b797aa1
harryfyjiswalker <harryfyjiswalker@gmail.com>
2025-01-18 00:32:13 +0000

    Create 4.4-soupornosoup.py

---
+++ Python Practice/University of Helsinki Python Programming MOOC/Part 1/4.4-soupornosoup.py
@@ -0,0 +1,7 @@
+name = str(input("Please tell me your name:"))
+if name == "Jerry":
+    print("Next please!")
+else:
+    soup = int(input("How many portions of soup?"))
+    print(f"The total cost is {soup*5.9}\nNext please!")
+    
