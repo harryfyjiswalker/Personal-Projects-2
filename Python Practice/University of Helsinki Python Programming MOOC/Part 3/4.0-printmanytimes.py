df82c9229a3a2a201209927e59accce2e71d4587
harryfyjiswalker <harryfyjiswalker@gmail.com>
2025-01-23 11:31:16 +0000

    Create 4.0-printmanytimes.py

---
+++ Python Practice/University of Helsinki Python Programming MOOC/Part 3/4.0-printmanytimes.py
@@ -0,0 +1,9 @@
+def print_many_times(text, times):
+    i = 0
+    while i < int(times):
+        print(text)
+        i +=1
+
+# You can test your function by calling it within the following block
+if __name__ == "__main__":
+    print_many_times("python", 5)
