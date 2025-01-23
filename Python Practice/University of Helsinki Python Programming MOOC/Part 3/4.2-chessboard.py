71b04faa3418c2726e419b8cef525e94a9b226f0
harryfyjiswalker <harryfyjiswalker@gmail.com>
2025-01-23 12:05:08 +0000

    Update 4.2-chessboard.py

--- Python Practice/University of Helsinki Python Programming MOOC/Part 3/4.2-chessboard.py
+++ Python Practice/University of Helsinki Python Programming MOOC/Part 3/4.2-chessboard.py
@@ -11,3 +11,17 @@ def chessboard(length):
 # Testing the function
 if __name__ == "__main__":
     chessboard(3)
+
+
+#alternative sol:
+def chessboard(size):
+    i = 0
+    while i < size:
+        if i % 2 == 0:
+            row = "10"*size
+        else:
+            row = "01"*size
+        # Remove extra characters at the end of the row
+        print(row[0:size])
+        i += 1
+ 
