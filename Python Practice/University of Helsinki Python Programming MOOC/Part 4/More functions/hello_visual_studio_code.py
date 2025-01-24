ddb01a1ef37f789f8aa28924d7b222bd3fec9ec0
harryfyjiswalker <harryfyjiswalker@gmail.com>
2025-01-24 13:31:44 +0100

    Create hello_visual_studio_code.py

---
+++ Python Practice/University of Helsinki Python Programming MOOC/Part 4/More functions/hello_visual_studio_code.py
@@ -0,0 +1,10 @@
+while True:
+    editor = input("Editor: ")
+
+    if editor.lower() == "notepad" or editor.lower() == "word":
+        print("awful")
+    elif editor.lower() == "visual studio code":
+        print("an excellent choice!")
+        break
+    else:
+        print("not good")
