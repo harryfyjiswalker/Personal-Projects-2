50a57733bda91ffe7b58c75b33cb0685ee75119e
harryfyjiswalker <harryfyjiswalker@gmail.com>
2025-01-24 13:37:34 +0100

    Update 1.1-line.py

--- Python Practice/University of Helsinki Python Programming MOOC/Part 4/More functions/1.1-line.py
+++ Python Practice/University of Helsinki Python Programming MOOC/Part 4/More functions/1.1-line.py
@@ -1,1 +1,9 @@
-
+# Write your solution here
+def line(num, string):
+    if string == "":
+        print("*"*int(num))
+    else:
+        print(string[0]*int(num))
+# You can test your function by calling it within the following block
+if __name__ == "__main__":
+    line(5, "x")
