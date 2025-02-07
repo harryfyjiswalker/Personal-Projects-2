23a2f9a6968a6c4911e126938961f5f2acd98229
harryfyjiswalker <harryfyjiswalker@gmail.com>
2025-02-07 16:00:14 +0000

    Create 1.6-itemsmultipliedby2.py

---
+++ Python Practice/University of Helsinki Python Programming MOOC/Part 5/References/1.6-itemsmultipliedby2.py
@@ -0,0 +1,11 @@
+def double_items(numbers: list):
+    new_list = []
+    for i in range(len(numbers)):
+        new_list.append(2*numbers[i])
+    return new_list
+
+if __name__ == "__main__":
+    numbers = [2, 4, 5, 3, 11, -4]
+    numbers_doubled = double_items(numbers)
+    print("original:", numbers)
+    print("doubled:", numbers_doubled)
