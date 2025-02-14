d9cadec49bc14723bc063885579055a0e2f73e5b
harryfyjiswalker <harryfyjiswalker@gmail.com>
2025-02-14 14:32:17 +0000

    Create 3.4-olderpeople.py

---
+++ Python Practice/University of Helsinki Python Programming MOOC/Part 5/Tuples/3.4-olderpeople.py
@@ -0,0 +1,17 @@
+def older_people(people:list, year:int):
+    new_list = []
+    for (name, age) in people:
+        if age < year:
+            new_list.append(name)
+    return new_list
+
+if __name__ == "__main__":
+    p1 = ("Adam", 1977)
+    p2 = ("Ellen", 1985)
+    p3 = ("Mary", 1953)
+    p4 = ("Ernest", 1997)
+    people = [p1, p2, p3, p4]
+
+    
+    older = older_people(people, 1979)
+    print(older)
