4417cd31189d826bf664f80d34c9d6f1a9aa8365
harryfyjiswalker <harryfyjiswalker@gmail.com>
2025-02-14 14:27:56 +0000

    Create 3.3-oldestperson.py

---
+++ Python Practice/University of Helsinki Python Programming MOOC/Part 5/Tuples/3.3-oldestperson.py
@@ -0,0 +1,26 @@
+def oldest_person(people: list):
+    new_list = []
+    for i in range(len(people)):
+        new_list.append(people[i][1])
+        minimum = min(new_list)
+        index = new_list.index(minimum)
+    return people[index][0]
+
+# def oldest_person(people: list):
+#     if people == []:
+#         return None
+#     oldest = people[0]
+#     for (name, age) in people[1:]:
+#         if age < oldest[1]:
+#             oldest = (name, age)
+#     return oldest[0]
+
+
+if __name__ == "__main__":
+    p1 = ("Adam", 1977)
+    p2 = ("Ellen", 1985)
+    p3 = ("Mary", 1953)
+    p4 = ("Ernest", 1997)
+    people = [p1, p2, p3, p4]
+
+    print(oldest_person(people))
