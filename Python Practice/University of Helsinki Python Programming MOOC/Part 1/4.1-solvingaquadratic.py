44245420cdbe54746517005587566bdd61b59609
harryfyjiswalker <harryfyjiswalker@gmail.com>
2025-02-20 09:41:47 +0000

    Update 4.1-solvingaquadratic.py

--- Python Practice/University of Helsinki Python Programming MOOC/Part 1/4.1-solvingaquadratic.py
+++ Python Practice/University of Helsinki Python Programming MOOC/Part 1/4.1-solvingaquadratic.py
@@ -1,11 +1,18 @@
 from math import sqrt
 
-a = float(input("a:"))
-b = float(input("b:"))
-c = float(input("c:"))
+# Taking input from the user
+a = float(input("a: "))
+b = float(input("b: "))
+c = float(input("c: "))
 
+# Calculate discriminant
 disc = b**2 - 4*a*c
 
-x_1 = (-b + sqrt(disc))/(2*a)
-x_2 = (-b - sqrt(disc))/(2*a)
-print(f"The roots are {x_1} and {x_2}.")
+# Check if discriminant is negative
+if disc < 0:
+    print("The equation has complex roots.")
+else:
+    # Calculate roots if discriminant is non-negative
+    x_1 = (-b + sqrt(disc)) / (2 * a)
+    x_2 = (-b - sqrt(disc)) / (2 * a)
+    print(f"The roots are {x_1} and {x_2}")
