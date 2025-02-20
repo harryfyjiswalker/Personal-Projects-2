bc03f07576959fdc491114cec162ec02ef3b575d
harryfyjiswalker <harryfyjiswalker@gmail.com>
2025-02-20 12:39:42 +0000

    Update 1.6-sudokugrid.py

--- Python Practice/University of Helsinki Python Programming MOOC/Part 5/More Lists/1.6-sudokugrid.py
+++ Python Practice/University of Helsinki Python Programming MOOC/Part 5/More Lists/1.6-sudokugrid.py
@@ -14,8 +14,8 @@ def sudoku_grid_correct(sudoku:list):
             c = sudoku[a].count(b)
             counting_row.append(c)
         counting_rows.append(counting_row)
-    for d in range(len(counting_row)):
-        if counting_row[d] >= 2:
+    for row in counting_rows:
+        if any(x >= 2 for x in row):
             evaluating_row.append(1)
         else:
             evaluating_row.append(0)
@@ -26,34 +26,28 @@ def sudoku_grid_correct(sudoku:list):
         for f in range(len(sudoku)):
             column.append(sudoku[f][e])
         columns.append(column)
-    for g in range(1,10):
-        h = column.count(g)
-        counting_col.append(h)
-    for h in range(len(counting_col)):
-        if counting_col[h] >= 2:
+    for column in columns:
+        if any(column.count(g) >= 2 for g in range(1,10)):
             evaluating_col.append(1)
         else:
-            evaluating_col.append(0)      
+            evaluating_col.append(0)    
     
     #blocks
 
-    for i in range(0, 7):  
-        for j in range(0, 7):  
+    for i in range(0, 7, 3):  
+        for j in range(0, 7, 3):  
             block = []
-            for k in range(i, i+3):  
-                for y in range(j, j+3):  
-                    block.append(sudoku[x][y])
-        blocks.append(block)
+            for k in range(i, i + 3):  
+                for y in range(j, j + 3):  
+                    block.append(sudoku[k][y])
+            blocks.append(block)
 
-    for k in range(1,10):
-        l = block.count(k)
-        counting_blo.append(l)
-    for m in range(len(counting_blo)):
-        if counting_blo[i] >=2:
+    for block in blocks:
+        if any(block.count(k) >= 2 for k in range(1, 10)):  
             evaluating_blo.append(1)
         else:
             evaluating_blo.append(0)
-    
+
     if 1 in evaluating_row or 1 in evaluating_col or 1 in evaluating_blo:
         return False
     else:
@@ -62,10 +56,10 @@ def sudoku_grid_correct(sudoku:list):
 
 
 
-if __name__ == "__main__":
-    sudoku1 = [
-  [9, 0, 0],
-  [2, 0, 0],
-  [0, 2, 0],
-]
-    print(sudoku_grid_correct(sudoku1))
+# if __name__ == "__main__":
+#     sudoku1 = [
+#   [9, 0, 0],
+#   [2, 0, 0],
+#   [0, 2, 0],
+# ]
+#     print(sudoku_grid_correct(sudoku1))
