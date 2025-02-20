5dfdda1db7ad697b0cccdad0d62bf2c4a5b7e750
harryfyjiswalker <harryfyjiswalker@gmail.com>
2025-02-20 11:50:37 +0000

    Update 2.0-firstsecondlast.py

--- Python Practice/University of Helsinki Python Programming MOOC/Part 4/More functions/2.0-firstsecondlast.py
+++ Python Practice/University of Helsinki Python Programming MOOC/Part 4/More functions/2.0-firstsecondlast.py
@@ -1,4 +1,3 @@
-# Write your solution here
 def first_word(sentence):
     i = 0
     while True:
@@ -6,25 +5,37 @@ def first_word(sentence):
             i+=1
         else:
             break
-    print(sentence[0:i])  
+    return sentence[0:i]
 
 def second_word(sentence):
-    i=0
-    j=i+1
-    while True:
-        if sentence[i] != " ":
-            i+=1
-        else:
-            i=i
-            if sentence[j] !=" ":
-                j+=1
-            else:
-                break
-    print(sentence[0:i])
+    list1=[]
+    indexes = []
+    for i in range(len(sentence)):
+        list1.append(sentence[i])
+    for i in range(len(list1)):
+        if list1[i] == ' ':
+            indexes.append(i)
+    if len(indexes) == 1:
+        secondword = list1[indexes[0]+1:len(sentence)]
+    else:
+        secondword = list1[indexes[0]+1:indexes[1]]
+    return ''.join(map(str, secondword))
+
+def last_word(sentence):
+    list2=[]
+    indexes1 = []
+    for i in range(len(sentence)):
+        list2.append(sentence[i])
+    for i in range(len(list2)):
+        if list2[i] == ' ':
+            indexes1.append(i)
+    lastword = list2[indexes1[-1]+1:len(sentence)]
+    return ''.join(map(str, lastword))
+
 
 # You can test your function by calling it within the following block
 if __name__ == "__main__":
     sentence = "once upon a time there was a programmer"
-    #print(first_word(sentence))
+    print(first_word(sentence))
     print(second_word(sentence))
-    #print(last_word(sentence))
+    print(last_word(sentence))
